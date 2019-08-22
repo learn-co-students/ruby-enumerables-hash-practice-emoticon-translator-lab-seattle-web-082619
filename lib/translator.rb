1c94ec5c398d01e186fd2202e2f32835aaf40810
Justin Maple <justinamaple@gmail.com>
2019-08-22 17:14:31 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,30 +3,29 @@ $ENG = 0
 $JAP = 1 
 
 def load_library(file_path)
-  lib = {
+  library = {
     "get_meaning" => {},
     "get_emoticon" => {}
   }
   yaml = YAML.load_file(file_path)
   
   yaml.each_pair do |meaning, emoticons|
-    lib["get_meaning"][emoticons[$JAP]] = meaning
-    lib["get_emoticon"][emoticons[$ENG]] = emoticons[$JAP]
+    library["get_meaning"][emoticons[$JAP]] = meaning
+    library["get_emoticon"][emoticons[$ENG]] = emoticons[$JAP]
   end
   
-  lib
+  library
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  lib = load_library(file_path)
-  
-  result = lib["get_emoticon"][emoticon]
-  result ? result : "Sorry, that emoticon was not found"
+  search(load_library(file_path), "get_emoticon", emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  lib = load_library(file_path)
-  
-  result = lib["get_meaning"][emoticon]
+  search(load_library(file_path), "get_meaning", emoticon)
+end
+
+def search(library, search_type, search_value)
+  result = library[search_type][search_value]
   result ? result : "Sorry, that emoticon was not found"
 end
